49406cdfc437595f3406a4bc9234528cdd7270a5
Mohammed <m.alsami@hotmail.com>
2022-04-09 17:15:44 +0100

    function returns desired data

--- src/javascript/WeatherData.js
+++ src/javascript/WeatherData.js
@@ -1,10 +1,18 @@
 // Api key is free tier
 let apiKey = "10b9d3fdb8bd7573f3137c7ef6e31701";
 
-async function getWeatherData(location){
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`,{mode: 'cors'})
-    const weatherData = await response.json();
-    console.log(weatherData)
+async function getWeatherData(location,units){
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=${units}`,{mode: 'cors'})
+    const weatherData = await response.json()
+    
+    const temp = weatherData.main.temp;
+    const windSpeed = weatherData.wind.speed;
+    const icon = weatherData.weather[0].icon;
+    const description = weatherData.weather[0].description;
+    return { temp, windSpeed, icon, description}
+    
+
+   
 }
 
 export default getWeatherData
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,4 +1,6 @@
 import '../sass/styles.scss';
 import getWeatherData from './WeatherData'
 
-console.log(getWeatherData("London"))
+
+
+
