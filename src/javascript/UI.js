08b7f2fc218f00c01587a92b402df93521a73111
Mohammed <m.alsami@hotmail.com>
2022-04-10 02:17:24 +0100

    basic UI started

--- src/javascript/UI.js
+++ src/javascript/UI.js
@@ -1,6 +1,11 @@
 import './Background'
+let container = document.createElement('div')
+container.className="container"
 let content = document.createElement('div')
 content.id="content"
+
 let appContainer = document.createElement('div')
 appContainer.id = 'app-container'
-document.body.appendChild(content)
+document.body.appendChild(container)
+container.appendChild(content)
+content.appendChild(appContainer)
