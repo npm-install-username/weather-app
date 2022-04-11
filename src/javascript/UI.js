ceb40e07cc1fbefadc2bb8ccb3eb5dd519b76dd0
Mohammed <m.alsami@hotmail.com>
2022-04-11 10:24:03 +0100

    added search bar with some basic styling

--- src/javascript/UI.js
+++ src/javascript/UI.js
@@ -1,11 +1,22 @@
 import './Background'
 let container = document.createElement('div')
 container.className="container"
+document.body.appendChild(container)
 let content = document.createElement('div')
 content.id="content"
+container.appendChild(content)
 
 let appContainer = document.createElement('div')
 appContainer.id = 'app-container'
-document.body.appendChild(container)
-container.appendChild(content)
 content.appendChild(appContainer)
+
+let searchBarDiv = document.createElement('div')
+searchBarDiv.id = "search-bar-div"
+let searchBar = document.createElement('input')
+searchBar.type = "text"
+searchBar.id = "search-bar"
+searchBar.name = "search-bar"
+searchBar.placeholder = "Enter a country or city..."
+appContainer.appendChild(searchBar)
+
+
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,6 +1,7 @@
+import './UI'
 import '../sass/styles.scss';
 import getWeatherData from './WeatherData'
-import './UI'
+
 
 
 
