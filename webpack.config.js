4dc91a5b327515c472bc59f412203754b9f2f601
Mohammed <m.alsami@hotmail.com>
2022-04-11 11:03:29 +0100

    added quicksand font

--- webpack.config.js
+++ webpack.config.js
@@ -65,6 +65,19 @@ module.exports = {
                    }
                  ]
                  
+          },
+          {
+            // Apply rule for fonts files
+            test: /\.(woff|woff2|ttf|otf|eot)$/,
+            use: [
+                   {
+                     // Using file-loader too
+                     loader: "file-loader",
+                     options: {
+                       outputPath: 'fonts'
+                     }
+                   }
+                 ]
           }
     ],
 
