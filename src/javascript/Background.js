c76b9617055588f1931e7363a625c8692ac21aed
Mohammed <m.alsami@hotmail.com>
2022-04-12 18:42:46 +0100

    image change still not working - maybe need a new config module

--- src/javascript/Background.js
+++ src/javascript/Background.js
@@ -1,8 +1,9 @@
 function changeBackground(data){
     let iconCode = data.icon
     let imageCode = iconCode.slice(0,-1)
-    console.log(imageCode)
-    document.getElementById('content').style.backgroundImage = "url(../images/04.jpg)"
+    console.log(imageCode);
+    let imgURL = "url('../images/02.jpg')"
+    document.getElementById('content').style.backgroundImage = imgURL;
 }
 
 export default changeBackground
